53d2e99143ecbc9bf149c7ad67a73242fa88b6ed
Divya Gate <divyagate543@gmail.com>
2026-06-14 13:15:46 +0530

    Remove .next.zip

--- app/loading.tsx
+++
@@ -1,9 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="text-xl font-semibold animate-pulse">
-        Loading...
-      </div>
-    </div>
-  );
-}
